fa4715d60d7aeabb6edafc57e72d8628389f78a9
lyodine <lyodine@users.noreply.github.com>
2024-09-02 18:13:26 -0400

    doc(core): Refactor with Flake8

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -6,4 +6,10 @@ from core.population import Individual, Population
 from core.selector import Elitist, SimpleSelector
 from core.variator import Variator
 
+__all__ = ["Controller",
+           "Evaluator",
+           "Individual", "Population",
+           "Elitist", "SimpleSelector",
+           "Variator",]
+
 # TODO Do I store stock implementations in these modules?
