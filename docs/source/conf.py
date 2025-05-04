d368989dce5d99219f61ad255a7db10584200ca4
lyodine <lycommit@outlook.com>
2025-05-04 00:36:11 -0400

    doc(api): Rebuild

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,15 +17,33 @@ sys.path.append(os.path.abspath(os.path.join('..', '..', 'src')))
 extensions = ['sphinx.ext.todo',
               'sphinx.ext.viewcode',
               'sphinx.ext.autodoc',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'sphinx.ext.autosummary']
 
-templates_path = ['_templates']
-exclude_patterns = []
+# -- Options for Typing ------------------------------------------------------
 
 language = 'en-uk'
+
+autoclass_content = 'class'
+autosummary_generate = True
+
+autodoc_default_options = {
+    'members':           True,
+    'undoc-members':     True,
+}
+autodoc_class_signature='separated'
+autodoc_inherit_docstrings=True
+autodoc_member_order = 'bysource'
+autodoc_typehints='signature'
+autodoc_typehints_description_target = 'all'
+
+napoleon_use_rtype = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+html_css_files = ['styles.css',]
 html_static_path = ['_static']
+templates_path = ['_templates']
+exclude_patterns = []
