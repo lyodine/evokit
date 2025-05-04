e04b4e7ee7befd563db79f30e3713491201abffb
lyodine <lycommit@outlook.com>
2025-05-04 00:31:32 -0400

    code(core): Add exports to `__init__.py`

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -4,8 +4,11 @@ from core.globals import LogLevel, report
 from core.controller import Controller
 from core.evaluator import Evaluator
 from core.population import Genome
+from core.variator import Variator
 from core.population import Population
 from core.population import GenomePool
+from core.selector import Elitist
+from core.selector import SimpleSelector
 
 # TODO: The modules Variator, Evaluator, Genome, etc. are abstract interfaces.
 #   A Java programmer might consider naming them, for example, AbstractVariator or AbstractEvaluator.
