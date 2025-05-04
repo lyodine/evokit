afdf36148a212cceea01ef58b716b527276cdead
lyodine <lycommit@outlook.com>
2025-05-04 00:36:06 -0400

    feat(core!): Completely deprecate logger

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,6 +1,5 @@
 """ Export modules from core.
 """
-from core.globals import LogLevel, report
 from core.controller import Controller
 from core.evaluator import Evaluator
 from core.population import Genome
