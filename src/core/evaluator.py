7f126f60cdca301b5277faaa4817e94a38b4d9c8
lyodine <lycommit@outlook.com>
2025-05-04 00:31:31 -0400

    feat(evaluator): Implement type checking

--- src/core/evaluator.py
+++ src/core/evaluator.py
@@ -8,14 +8,53 @@ from .population import Genome
 
 T = TypeVar("T", bound=Genome)
 
-# NOTE: From a Java background, I am used to creating a exception type for each exceptional case that may arise.
-class ScoringException(Exception):
-    """This exception arises from exceptions thrown during the scoring process.
-        TODO: The only place that raises this exception, as of 2024-04-05, is
-            when the thing that is being scored is not a genome.
-        Solution: try to create a exception class that matches
-            "operator type mismatch"
+
+
+class _MetaEvaluator(abc.ABCMeta):
+    """Machineary for the evaluator.
+        Because ABC (abstract base class) is also implemented with metaclasses,
+            and a class cannot have two metaclasses, I must implement the following:
+            (1) MetaEvaluator extends ABCMeta
+            (2) Evaluator has MetaEvaluator as metaclass, thereby indirectly
+                using ABCMeta as metaclass
+            (3) Evaluator extends abc.ABC, which has ABCMeta as metaclass.
+            (4) Things work out, because both "chains" lead to the same
+                metaclass, which is ABCMeta.
+
+        ABCMeta <|-- MetaEvaluator
+        ^               ^
+        | <metaclass>   | <metaclass>
+        |               |
+        abc.ABC <|-- Evaluator
+
     """
+    # cls, name, bases, attrs
+    def __new__(mcls, name, bases, namespace):
+        """!Python magic. This is only moderately cursed: one gets used to
+            metaclasses after a while.
+            This {decorator/metaclass} modifies the behaviour of a evaluator.
+        """
+        def wrap_function(custom_evaluate):
+            def wrapper(*args, **kwargs) -> float:
+                genome = args[1]
+                if not isinstance(genome, Genome):
+                    raise TypeError("Evaluator is not a genome")
+                elif genome.is_scored():
+                    print("no shortcut!")
+                    return genome.score
+                else:
+                    print("shortcut!")
+                    score: float = custom_evaluate(*args, **kwargs)
+                    genome.score = score
+                    return score
+            return wrapper
+        
+        if name != "Evaluator":
+            namespace["evaluate"] = wrap_function(
+                namespace.setdefault("evaluate", lambda: None)
+            )
+        # This is necessary. Because __new__ is 
+        return type.__new__(mcls, name, bases, namespace)
 
 
 class Evaluator(abc.ABC, Generic[T]):
@@ -25,11 +64,11 @@ class Evaluator(abc.ABC, Generic[T]):
     def evaluate_shortcut(func):
         """!Apply the "dynamic scoring" heuristic to the evaluate(.) method.
         """
-
         def wrapper(*args, **kwargs) -> float:
+            print ("shortcut used")
             genome = args[1]
             if not isinstance(genome, Genome):
-                raise ScoringException("This thing is not a genome!")
+                raise TypeError("Evaluator is not a genome")
             elif genome.is_scored():
                 return genome.score
             else:
@@ -38,25 +77,16 @@ class Evaluator(abc.ABC, Generic[T]):
                 return score
         return wrapper
 
-
     @abc.abstractmethod
-    @evaluate_shortcut
     def evaluate(self: Self, s1: T)-> float:
         """!Evaluate an individual and return the score.
             Higher scores are better.
         """
 
-
-    # TODO not implemented. Should I let the evaluator directly report truncation? If so,
-    #   then this method belongs here; otherwise, remote it completely, and move
-    #   the reporting mechanism to the genome (as a field)
-    # @abc.abstractmethod
-    # def truncate(self: Self) -> bool:
-    #     """Return if a termination condition has been reached.
-    #     """
-    #     return False
-    def evaluate_population(self: Self, pop: Population[T])-> Population[T]:
-        """!Score every individual of a population
+    def evaluate_population(self: Self,
+                            pop: Population[T])-> Population[T]:
+        """!Iterate through genomes in a population. For each genome,
+            assign to it a fitness.
         """
         for x in pop:
             x.score = self.evaluate(x)
