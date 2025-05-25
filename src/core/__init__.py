c0f8cc26ccb98346539b090326ca8753fd13a333
lyodine <commit@lyodine.com>
2025-05-24 21:44:00 -0400

    code(core!): Remove exports in `__all__` At this stage, having explicitly exports in each `__init__.py` makes it difficult to move things around and nail down import errors. Best to export directly from submodules.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -6,10 +6,4 @@ from core.population import Individual, Population
 from core.selector import Elitist, SimpleSelector
 from core.variator import Variator
 
-__all__ = ["Controller",
-           "Evaluator",
-           "Individual", "Population",
-           "Elitist", "SimpleSelector",
-           "Variator",]
-
 # TODO Do I store stock implementations in these modules?
