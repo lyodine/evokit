6869226c7337f77d4895faf987d5d3749a0bc3ff
lyodine <commit@lyodine.com>
2025-05-24 21:44:55 -0400

    code(selectors): Update implementations
    Necessary after prior breaking change.

--- evokit/evolvables/selectors.py
+++ evokit/evolvables/selectors.py
@@ -5,14 +5,16 @@ from ..core import Individual
 import random
 
 from typing import Self
+
 from typing import Any
 from typing import TypeVar
-from typing import Optional
+from typing import Sequence
 from typing import Callable
 from typing import override
 from types import MethodType
 from functools import wraps
 
+from operator import attrgetter
 
 D = TypeVar("D", bound=Individual)
 
@@ -25,102 +27,69 @@ class NullSelector(Selector[D]):
         pass
 
     @override
-    def select(self: Self, *_: Any, **__: Any) -> Any:
-        pass
-
-    @override
-    def select_to_many(self, population: Population[D]) -> tuple[D, ...]:
-        """Select every item in the population.
+    def select_population(self: Self,
+                          from_population: Population[D]) -> Population[D]:
+        """Return all items in :arg:`from_population` in  new population.
         """
-        return tuple(x for x in population)
+        return Population(*from_population)
 
 
 class TruncationSelector(Selector[D]):
-    """Simple selector that select the highest-fitness individual.
+    """Simple selector that select individuals with highest fitness.
     """
     @override
     def __init__(self: Self, budget: int):
         super().__init__(budget)
 
-    def select(self,
-               population: Population[D]) -> tuple[D]:
-        """Greedy selection.
-
-        Select the item in the population with highest fitness.
-        """
-        population.sort(lambda x: x.fitness)
-        selected_solution = population[0]
-        return (selected_solution,)
-
-
-class ElitistTruncationSelector(TruncationSelector[D]):
-    """Elitist selector that select the highest-fitness individual.
-
-    Example for overriding `select_to_many`. Just overriding `select`
-        is not enough, because elitism requires the highest-fitness
-        individual of a _population_.
-    """
     @override
-    def __init__(self: Self, budget: int):
-        super().__init__(budget - 1)
-        self.best_individual: Optional[D] = None
-
-    @override
-    def select_to_many(self, population: Population[D]) -> tuple[D, ...]:
-        """Context that implements elitism.
-
-        Preserve and update an elite. Each time the selector is used, insert
-        the current elite to the results.
-        """
-        results: tuple[D, ...] = super().select_to_many(population)
-        best_individual: Optional[D] = self.best_individual
-        if best_individual is None:
-            best_individual = results[0]
-        for x in results:
-            if x.fitness < best_individual.fitness:
-                best_individual = x
-        self.best_individual = best_individual
-
-        return (*results, self.best_individual)
+    def select_population(self: Self,
+                          from_population: Population[D]) -> Population[D]:
+        return Population(*sorted(from_population, key=attrgetter("fitness"))
+                          [-self.budget:])
 
 
 class TournamentSelector(Selector[D]):
-    """Tournament selector.
+    """Tournament selector:
+
+    #. From the population, select uniform sample of size :arg:`bracket_size`.
+    #. Iterate through the sample, stop when a selection is made.
+        At index i, select the ith item with probability :math:`p * (1- p)^i`.
+        If no selection is made when reaching the end of the sample, select
+        the last item.
+    #. Repeat until :arg:`budget` items are selected.
     """
     def __init__(self: Self, budget: int, bracket_size: int = 2,
-                 probability: float = 1):
+                 p: float = 1):
         super().__init__(budget)
         self.bracket_size: int = bracket_size
-        self.probability: float = min(2, max(probability, 0))
+        self.p: float = min(2, max(p, 0))
 
     @override
-    def select(self,
-               population: Population[D]) -> tuple[D]:
+    def select(self, from_pool: Sequence[D]) -> tuple[D]:
         """Tournament selection.
 
         Select a uniform sample, then select the best member in that sample.
         """
-        # Do not select if
-        #   (a) the sample is less than bracket_size, or
-        #   (b) the budget is less than bracket_size
         sample: list[D]
-        if min(len(population), self.budget) < self.bracket_size:
-            sample = list(population)
+
+        budget_cap: int = min(len(from_pool), self.budget)
+
+        # Ensure: the size of a sample must not exceed the output arity.
+        if budget_cap < self.bracket_size:
+            sample = list(from_pool)
         else:
-            sample = random.sample(tuple(population), self.bracket_size)
+            # For some reason `random.sample` returns a list.
+            sample = random.sample(tuple(from_pool), self.bracket_size)
+
         sample.sort(key=lambda x: x.fitness, reverse=True)
 
-        # If nothing is selected stochastically, select the last element
-        selected_solution: D = sample[-1]
-
-        # Select the ith element with probability p * (1-p)**i
-        probability = self.probability
+        # Iterate items, select each with probability p * (1 - p)**i.
         for i in range(len(sample)):
-            if random.random() < probability * (1 - probability)**i:
-                selected_solution = sample[i]
-                break
+            if random.random() < self.p * (1 - self.p)**i:
+                return (sample[i],)
 
-        return (selected_solution,)
+        # If nothing is selected in the end, select the last element
+        return (sample[-1],)
 
 
 def Elitist(sel: Selector[D]) -> Selector:
@@ -130,8 +99,8 @@ def Elitist(sel: Selector[D]) -> Selector:
     Each time the selector is called, append that individual to the end
     of the output population.
 
-    Modify `select_to_many` of `sel` to use elitism. If `sel` already
-        overrides `select_to_many`, that implementation is destroyed.
+    Modify `select_population` of `sel` to use elitism. If `sel` already
+        overrides `select_population`, that implementation is destroyed.
 
     Args:
         sel: A selector
@@ -140,14 +109,14 @@ def Elitist(sel: Selector[D]) -> Selector:
         A selector
     """
 
-    def wrap_function(original_select_to_many:
+    def wrap_function(original_select_population:
                       Callable[[Selector[D], Population[D]],
-                               tuple[D, ...]]) -> Callable:
+                               Population[D]]) -> Callable:
 
-        @wraps(original_select_to_many)
+        @wraps(original_select_population)
         def wrapper(self: Selector[D],
                     population: Population[D],
-                    *args: Any, **kwargs: Any) -> tuple[D, ...]:
+                    *args: Any, **kwargs: Any) -> Population[D]:
             """Context that implements elitism.
             """
             population_best: D = population.best()
@@ -157,30 +126,32 @@ def Elitist(sel: Selector[D]) -> Selector:
             # This attribute retains the HOF individual.
             # Current name is taken from a randomly generated SSH pubkey.
             #   Nobody else will use a name *this* absurd.
-            UBER_SECRET_BEST_INDIVIDUAL_NAME =\
-                "___g1AfoA2NMh8ZZCmRJbwFcne4jS1f3Y2TRPIvBmVXQP"
-            if not hasattr(self, UBER_SECRET_BEST_INDIVIDUAL_NAME):
-                setattr(self, UBER_SECRET_BEST_INDIVIDUAL_NAME, population_best.copy())
+            BEST_INDIVIDUAL_ATTR_NAME =\
+                "___g1AfoA2NMh8ZZCmRJbweeee4jS1f3Y2TRPIvBmVXQP"
+            if not hasattr(self, BEST_INDIVIDUAL_ATTR_NAME):
+                setattr(self, BEST_INDIVIDUAL_ATTR_NAME,
+                        population_best.copy())
 
             hof_individual: D
-            my_best = getattr(self, UBER_SECRET_BEST_INDIVIDUAL_NAME)
+            my_best = getattr(self, BEST_INDIVIDUAL_ATTR_NAME)
 
             if my_best.fitness > population_best.fitness:
                 hof_individual = my_best
             else:
                 hof_individual = population_best
-                setattr(self, UBER_SECRET_BEST_INDIVIDUAL_NAME, population_best.copy())
+                setattr(self, BEST_INDIVIDUAL_ATTR_NAME,
+                        population_best.copy())
 
             # Acquire results of the original selector
-            results: tuple[D, ...] = \
-                original_select_to_many(self, population, *args, **kwargs)
+            results: Population[D] = \
+                original_select_population(self, population, *args, **kwargs)
 
             # Append the best individual to results
-            return (*results, hof_individual.copy())
+            return Population(*results, hof_individual.copy())
         return wrapper
 
-    setattr(sel, 'select_to_many',
+    setattr(sel, 'select_population',
             MethodType(
-                wrap_function(sel.select_to_many.__func__),  # type:ignore
+                wrap_function(sel.select_population.__func__),  # type:ignore
                 sel))
     return sel
