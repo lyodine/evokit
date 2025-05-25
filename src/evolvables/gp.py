f0015ee93057c002e6e6e2ebb3294ca742af5b9b
lyodine <commit@lyodine.com>
2025-05-24 21:43:58 -0400

    style(gp): Refactor with Flake8

--- src/evolvables/gp.py
+++ src/evolvables/gp.py
@@ -11,6 +11,8 @@ if TYPE_CHECKING:
     from typing import Self
     from typing import Callable
 
+from core import Variator
+
 import abc
 import functools
 import math
@@ -20,25 +22,18 @@ from inspect import signature
 from random import choice
 from typing import Generic
 
-import gymnasium as gym
+from core import Evaluator, Individual
 
-from core import (Controller, Elitist, Evaluator, Individual, Population,
-                  SimpleSelector, Variator)
 
 T = typing.TypeVar("T")
 
-class ArityMismatch(TypeError):
-    def __init__(self, expr, expected:Optional[int], given: Optional[int]):
-        super().__init__(f"function {str(expr)} expects "
-                         f"{expected} arguments, got"
-                         f"{given}.")
 
 def _get_arity(fun: Any) -> int:
     """Inspect the arity of an object
 
     Return the signatue length of a callable. If the input is not callable,
         return 0.
-    
+
     Args:
         fun: An object
 
@@ -46,18 +41,28 @@ def _get_arity(fun: Any) -> int:
         The arity of `fun`
     """
     if (callable(fun)):
-        return len(signature(fun).parameters)                 
+        return len(signature(fun).parameters)
     elif isinstance(fun, Program):
         # Specialised code for programs
         return len(fun.symbols)
     else:
         return 0
 
+
+class ArityMismatch(TypeError):
+    def __init__(self, expr, expected: Optional[int], given: Optional[int]):
+        super().__init__(f"function {str(expr)} expects "
+                         f"{expected} arguments, got"
+                         f"{given}.")
+
+
 class Expression(abc.ABC, typing.Generic[T]):
+    """Data structure of a program tree.
+    """
     def __init__(self, function: T | typing.Callable[..., T], *children: Expression[T]):
         self._function = function
-        self.children : List[Expression[T]] = list(children)
-    
+        self.children: List[Expression[T]] = list(children)
+
     def evaluate(self) -> T:
         expected_arity = _get_arity(self._function)
         children_arity = len(self.children)
@@ -66,45 +71,43 @@ class Expression(abc.ABC, typing.Generic[T]):
             if (expected_arity == children_arity):
                 return self._function(*results)
             else:
-                raise ArityMismatch(self._function, expected_arity, children_arity)
+                raise TypeError(self._function, expected_arity, children_arity)
         elif expected_arity == 0 and children_arity == 0:
             return self._function
         else:
-            raise ArityMismatch(self._function, expected_arity, children_arity)
-        
-        return self._function(*results)
-    
+            raise TypeError(self._function, expected_arity, children_arity)
+
     def copy(self) -> Self:
         """ Copying self does not copy the funciton. """
-        children_copies : Tuple[Expression[T], ...] = tuple(x.copy() for x in self.children)
+        children_copies: Tuple[Expression[T], ...] =\
+            tuple(x.copy() for x in self.children)
         return self.__class__(self._function, *children_copies)
-    
+
     def nodes(self) -> List[Expression[T]]:
         return [self] + list(self.children)
-    
+
     def __str__(self) -> str:
         delimiter = ", "
 
-        my_name: str = self._function.__name__ if callable(self._function) else str(self._function)
+        my_name: str = self._function.__name__\
+            if callable(self._function) else str(self._function)
 
         children_name: str
         if len(self.children) < 1:
             children_name = ""
         else:
-            children_name = f"({functools.reduce(lambda x, y: str(x) + ", " + str(y), [str(x) for x in self.children])})"
-        
-        
-        return (f"{my_name}{children_name}")
-    
+            children_name = f"({functools.reduce(
+                lambda x, y: str(x) + delimiter + str(y),
+                [str(x) for x in self.children])})"
 
-    __copy__ = copy
-    __deepcopy__ = copy
+        return (f"{my_name}{children_name}")
 
 
 class ExpressionFactory(typing.Generic[T]):
     def __init__(self, functions: Tuple[Callable[..., T], ...]):
-        # Build a pool of functions, as a dictionary where each index corresponds to an arity, and each 
-        #   value is a list of functions (or terminals!) with that arity.
+        # Build a pool of functions, as a dictionary where each index
+        #   corresponds to an arity, and each value is a list of functions
+        #   (or terminals!) with that arity.
         self.function_pool: Dict[int, List[Callable[..., T]]] = {}
         for fun in functions:
             arity: int = _get_arity(fun)
@@ -118,33 +121,36 @@ class ExpressionFactory(typing.Generic[T]):
         self.depth = 0
         self.budget_used = 0
 
-    def build(self, depth: int, budget: int, nullary_ratio: Optional[float] = None) -> Expression:
+    def build(self, depth: int,
+              budget: int,
+              nullary_ratio: Optional[float] = None) -> Expression:
         self.budget_cap = budget
         self.budget_used = 0
 
         target_function = self.poll_function(nullary_ratio)
         arity = _get_arity(target_function)
 
-        children : List [Expression]= []
+        children: List[Expression] = []
         for i in range(0, arity):
             children.append(self._build_recurse(depth-1, nullary_ratio))
 
         root = Expression(target_function, *children)
         return root
-        
 
-    def _build_recurse(self, depth_left: int, nullary_ratio: Optional[float] = None) -> Expression:
+    def _build_recurse(self,
+                       depth_left: int,
+                       nullary_ratio: Optional[float] = None) -> Expression:
         if (depth_left < 1 or self.over_budget()):
             target_function = self.poll_arity(0)
             return Expression(target_function)
         else:
-            
+
             target_function = self.poll_function(nullary_ratio)
             arity = _get_arity(target_function)
-            children : List [Expression]= []
+            children: List[Expression] = []
             for i in range(0, arity):
                 children.append(self._build_recurse(depth_left-1, nullary_ratio))
-            random.shuffle (children)
+            random.shuffle(children)
             base = Expression[T](target_function, *children)
             return base
 
@@ -153,9 +159,9 @@ class ExpressionFactory(typing.Generic[T]):
 
     def cost_budget(self) -> None:
         self.budget_used = self.budget_used + 1
-        # report(LogLevel.TRC, f"budget used: {self.budget_used - 1} -> {self.budget_used}")
-    
-    def poll_function(self, nullary_ratio:Optional[float] = None) -> Callable[..., T]:
+
+    def poll_function(self,
+                      nullary_ratio: Optional[float] = None) -> Callable[..., T]:
         self.cost_budget()
         choice_poll: List[Callable[..., T]]
         if (nullary_ratio is None):
@@ -164,14 +170,16 @@ class ExpressionFactory(typing.Generic[T]):
             if (random.random() < nullary_ratio):
                 choice_poll = self.function_pool[0]
             else:
-                choice_poll = choice([self.function_pool[x] for x in self.function_pool.keys() if x != 0])            
-            
+                choice_poll = choice(
+                    [self.function_pool[x] for x in self.function_pool.keys() if x != 0])
+
         choice_function: Callable[..., T] = choice(choice_poll)
 
         return choice_function
 
     def poll_arity(self, arity: int) -> Callable[..., T]:
         return choice(self.function_pool[arity])
+
 
 class BadSymbolError(Exception):
     def __init__(self, name: str):
@@ -180,26 +188,28 @@ class BadSymbolError(Exception):
 
 class Symbol(typing.Generic[T]):
     def __init__(self, name: str = "default_symbol_name", value: Optional[T] = None):
-        self.value : Optional[T] = value
-        self.__name__ : str = name
+        self.value: Optional[T] = value
+        self.__name__: str = name
 
     def __call__(self) -> T:
         if (self.value is None):
             raise BadSymbolError(self.__name__)
         return self.value
-    
+
     def assign(self, val: T) -> None:
         self.value = val
-    
-    def __str__(self)-> str:
+
+    def __str__(self) -> str:
         return self.__name__
+
 
 class ProgramFactory(Generic[T]):
     def __init__(self, functions: Tuple[Callable[..., T], ...], arity):
         self.arity = arity
-        self._symbol_count = 0 # only used to keep track of symbol names. Does not relate to arity
-        self.symbol_deposit : List[Symbol]= []
-        
+        self._symbol_count = 0  # only used to keep track of symbol names.
+        # Does not relate to arity
+        self.symbol_deposit: List[Symbol] = []
+
         for i in range(0, arity):
             self.deposit_symbol(self.next_symbol())
 
@@ -211,91 +221,109 @@ class ProgramFactory(Generic[T]):
 
     def deposit_symbol(self, s: Symbol[T]) -> None:
         self.symbol_deposit.append(s)
-        
+
     def next_symbol(self) -> Symbol[T]:
         return Symbol("sym_" + self.next_symbol_name())
-    
-    def build(self, depth: int, budget: int, unary_weight: Optional[float] = None) -> Program:
-        return Program(self.exprfactory.build(depth, budget, unary_weight), self.symbol_deposit, self)
+
+    def build(self, depth: int,
+              budget: int,
+              unary_weight: Optional[float] = None) -> Program:
+        return Program(self.exprfactory.build(depth, budget, unary_weight),
+                       self.symbol_deposit,
+                       self)
 
 # Note that programs from the same factory share the same set of argument values.
 # This should be desirable - and well hidden - if the object is only shared by one thread.
 # I'm not prepared to deal with concurrency.
 
+
 class ProgramArityMismatchError(Exception):
-    def __init__(self, expected:Optional[int], given: Optional[int]):
-        super().__init__(f"The program is expecting {expected} arguments, only {given} are given.")
-        
+    def __init__(self, expected: Optional[int], given: Optional[int]):
+        super().__init__(f"The program is expecting {expected} arguments,"
+                         f"only {given} are given.")
+
+
 class Program(Individual[T]):
     """
-    
+
     """
     def __init__(self, expr: Expression, symbols: List[Symbol], factory: ProgramFactory):
         super().__init__()
         self.expr = expr
         self.symbols = symbols
         self.factory = factory
-    
+
     def evaluate(self, *args: T) -> T:
         if (len(args) != len(self.symbols)):
             raise ProgramArityMismatchError(len(self.symbols), len(args))
         self.__class__._assign_values(self.symbols, args)
         return self.expr.evaluate()
-    
+
     def __call__(self, *args: T) -> T:
         return self.evaluate(*args)
 
     @staticmethod
     def _assign_values(symbols: List[Symbol[T]], values: Tuple[T, ...]) -> None:
         # This is exceptionally unpythonic
-        for i in range (0, len(symbols)):
+        for i in range(0, len(symbols)):
             symbols[i].assign(values[i])
 
     def __str__(self) -> str:
         return str(self.expr)
-    
+
     # Warning! Breaking design patterns.
     def nodes(self) -> List[Expression[T]]:
         return self.expr.nodes()
-    
+
     def copy(self) -> Self:
         return self.__class__(self.expr.copy(), self.symbols, self.factory)
+
 
 def sin(x: float) -> float:
     return math.sin(x)
 
+
 def cos(x: float) -> float:
     return math.cos(x)
+
 
 def tan(x: float) -> float:
     return math.tan(x)
 
-def add (x:float, y:float):
+
+def add(x: float, y: float):
     return x + y
 
-def sub (x:float, y:float):
+
+def sub(x: float, y: float):
     return x - y
 
-def mul (x:float, y:float):
+
+def mul(x: float, y: float):
     return x * y
 
-def div (x:float, y:float):
+
+def div(x: float, y: float):
     if y == 0:
         return 1
     return x / y
 
-def avg (x:float, y:float):
-    return (x+y)/2
 
-def lim(x: float, max_val:float, min_val:float) -> float:
+def avg(x: float, y: float):
+    return (x + y)/2
+
+
+def lim(x: float, max_val: float, min_val: float) -> float:
     return max(min(max_val, x), min_val)
 
 
 class ProgramCrossoverVariator(Variator[Program[float]]):
-    def vary(self, parents: Tuple[Program[float], ...]) -> Tuple[Program[float], ...]:
+    def vary(self,
+             parents: Tuple[Program[float], ...]) -> Tuple[Program[float], ...]:
         return (self.crossover(parents) + self.mutate(parents))
 
-    def crossover(self, parents: Tuple[Program[float], ...]) -> Tuple[Program[float], ...]:
+    def crossover(self,
+                  parents: Tuple[Program[float], ...]) -> Tuple[Program[float], ...]:
         root1: Program = parents[0].copy()
         root2: Program = parents[1].copy()
 
@@ -303,17 +331,24 @@ class ProgramCrossoverVariator(Variator[Program[float]]):
         expression_nodes_from_root_2 = root2.nodes()
 
         # Select internal nodes - nodes that receive arguments.
-        expression_internal_nodes_from_root_1 = [x for x in expression_nodes_from_root_1 if len(x.children) > 0]
-        expression_internal_nodes_from_root_2 = [x for x in expression_nodes_from_root_2 if len(x.children) > 0]
+        expression_internal_nodes_from_root_1 =\
+            [x for x in expression_nodes_from_root_1 if len(x.children) > 0]
+        expression_internal_nodes_from_root_2 =\
+            [x for x in expression_nodes_from_root_2 if len(x.children) > 0]
 
-        # If both expression trees have valid internal nodes, 
-        if (len(expression_internal_nodes_from_root_1) >= 1 and len(expression_internal_nodes_from_root_2) >= 1):
-            expression_node_from_root_1_to_swap = random.choice(expression_internal_nodes_from_root_1)
-            expression_node_from_root_2_to_swap = random.choice(expression_internal_nodes_from_root_2)
-            self.__class__.swap_children(expression_node_from_root_1_to_swap, expression_node_from_root_2_to_swap)
-        print (str(root1) + "-----" + str(root2))
+        # If both expression trees have valid internal nodes,
+        if (len(expression_internal_nodes_from_root_1) >=
+                1 and len(expression_internal_nodes_from_root_2) >= 1):
+            expression_node_from_root_1_to_swap =\
+                random.choice(expression_internal_nodes_from_root_1)
+            expression_node_from_root_2_to_swap =\
+                random.choice(expression_internal_nodes_from_root_2)
+            self.__class__.swap_children(
+                expression_node_from_root_1_to_swap, expression_node_from_root_2_to_swap)
+
+        print(str(root1) + "-----" + str(root2))
         return (root1, root2)
-    
+
     def mutate(self, parents: Tuple[Program[float], ...]) -> Tuple[Program[float], ...]:
         root: Program = parents[0].copy()
         root.expr._function = self.__class__._draw_replacement_function(root)
@@ -324,20 +359,18 @@ class ProgramCrossoverVariator(Variator[Program[float]]):
         child_nodes = list(expr1.children + expr2.children)
         random.shuffle(child_nodes)
 
-        for i in range(0,len(expr1.children)):
+        for i in range(0, len(expr1.children)):
             expr1.children[i] = child_nodes[i].copy()
 
-        for i in range(-1,-(len(expr2.children) + 1), -1):
+        for i in range(-1, -(len(expr2.children) + 1), -1):
             expr2.children[i] = child_nodes[i].copy()
 
     @staticmethod
-    def _draw_replacement_function(program: Program)-> Callable:
+    def _draw_replacement_function(program: Program) -> Callable:
         source_factory = program.factory
         arity = _get_arity(program.expr._function)
         functions = source_factory.exprfactory.function_pool[arity]
         return random.choice(functions)
-
-
 
 
 class SymbolicEvaluator(Evaluator[Program[float]]):
@@ -353,8 +386,13 @@ class SymbolicEvaluator(Evaluator[Program[float]]):
             raise TypeError("what")
         return -sum([abs(self.objective(*sup) - program(*sup)) for sup in self.support])
 
+
 class GymEvaluator(Evaluator[Program[float]]):
-    def __init__(self, env, wrapper: Callable[[float], float], episode_count: int, step_count: int, score_wrapper: Callable[[float], float] = lambda x : x):
+    def __init__(self, env,
+                 wrapper: Callable[[float], float],
+                 episode_count: int,
+                 step_count: int,
+                 score_wrapper: Callable[[float], float] = lambda x: x):
         super().__init__()
         self.env = env
         self.wrapper = wrapper
@@ -363,29 +401,33 @@ class GymEvaluator(Evaluator[Program[float]]):
         self.score_wrapper = score_wrapper
 
     def evaluate(self, s1: Program[float]) -> float:
-        score = s1.evaluate(1,4,8,16)
-        #score = GymEvaluator.evaluate_episode(s1, self.env, self.wrapper, self.episode_count, self.step_count) 
+        score = s1.evaluate(1, 4, 8, 16)
         return self.score_wrapper(score)
 
     @staticmethod
-    def evaluate_episode(s1: Program[float], env, wrapper: Callable[[float], float], episode_count: int, step_count: int) -> float:
+    def evaluate_episode(s1: Program[float],
+                         env,
+                         wrapper: Callable[[float], float],
+                         episode_count: int,
+                         step_count: int) -> float:
         score = 0.
         for i in range(0, episode_count):
             score = score + GymEvaluator.evaluate_step(s1, env, wrapper, step_count)
         return score / episode_count
 
     @staticmethod
-    def evaluate_step(s1: Program[float], env, wrapper: Callable[[float], float], step_count: int) -> float:
+    def evaluate_step(s1: Program[float],
+                      env,
+                      wrapper: Callable[[float], float],
+                      step_count: int) -> float:
         step_result = env.reset()
         score = 0.
         # hard coded - an episode consists of 10 evaluations.
         for i in range(0, step_count):
-            if (len(step_result)>=5 and (step_result[2] or step_result[3])):
+            if (len(step_result) >= 5 and (step_result[2] or step_result[3])):
                 break
-            step_result = env.step(wrapper(s1.evaluate(*step_result[0]))) #type: ignore
+            step_result = env.step(wrapper(s1.evaluate(*step_result[0])))  # type: ignore
             if (step_result[2]):
                 break
-            score = score + step_result[1] #type: ignore
+            score = score + step_result[1]  # type: ignore
         return score
-
-
