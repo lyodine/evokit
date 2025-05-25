7328fbdc3f6226b6cf421ff0660a4c3d750a1d6f
lyodine <commit@lyodine.com>
2025-05-24 21:43:55 -0400

    style(binstring): Refactor with Flake8

--- src/evolvables/binstring.py
+++ src/evolvables/binstring.py
@@ -4,8 +4,11 @@ from typing import Optional, Tuple, TypeVar
 
 from core.controller import LinearController
 from core.evaluator import Evaluator
-from core.population import Individual, Population
-from core.selector import Elitist, SimpleSelector, TournamentSelector
+from core.population import Individual
+from core.population import Population
+from core.selector import Elitist
+from core.selector import SimpleSelector
+
 from core.variator import Variator
 
 
@@ -13,17 +16,19 @@ class IllegalVariation(Exception):
     def __init__(self):
         super().__init__("Given index out of bound!")
 
+
 T = TypeVar('T', bound=Individual)
 
+
 class Binary(Individual[T]):
-    def __init__(self, len: int, value: typing.Optional[int] = None) -> None: 
+    def __init__(self, len: int, value: typing.Optional[int] = None) -> None:
         super().__init__()
         self.length = len
         if (value is None):
             self._value = 0
         else:
             self._value = value
-    
+
     def _assert_index_is_valid(self, pos: int):
         if pos >= self.length:
             raise IllegalVariation
@@ -39,17 +44,16 @@ class Binary(Individual[T]):
     # def get (self, pos: int)-> int :
     #     self._assert_index_is_valid(pos)
     #     return (self._value >> pos) & 1
-    
+
     def toggle(self, pos: int):
         self._assert_index_is_valid(pos)
         self._value = (self._value ^ (1 << (pos)))
 
-
     def __len__(self) -> int:
         return self.length
-    
+
     @classmethod
-    def create_random(cls, len:int) -> typing.Self:
+    def create_random(cls, len: int) -> typing.Self:
         bin = cls(len)
         for i in range(0, len):
             if (bool(random.getrandbits(1))):
@@ -59,11 +63,11 @@ class Binary(Individual[T]):
     @property
     def value(self):
         return self._value
-    
+
     def copy(self) -> typing.Self:
         new_copy = self.__class__(self.length, self.value)
         return new_copy
-    
+
     __deepcopy__ = copy
     __copy__ = copy
 
@@ -71,28 +75,31 @@ class Binary(Individual[T]):
         # return str(bin(self._value))[2:].ljust(self.length, '0')
         return str(bin(self._value))[2:].rjust(self.length, '0')
 
+
 class BitDistanceEvaluator(Evaluator[Binary]):
     def evaluate(self, s1: Binary) -> float:
         return s1._value.bit_count()
 
+
 class RandomBitMutator(Variator[Binary]):
-    def __init__ (self):
+    def __init__(self):
         super().__init__(1, 2)
 
     def vary(self, parents: Tuple[Binary, ...], **kwargs) -> Tuple[Binary, ...]:
         binary = parents[0].copy()
         newbits = parents[0].copy()
-        
-        for i in range(0, len(binary)): # Somehow cannot properly implement the __len__ dunder 
+
+        for i in range(0, len(binary)):  # Somehow cannot properly implement __len__
             if (random.random()<0.001):
                 binary.toggle(i)
                 newbits.toggle(i)
         return (binary, newbits)
 
+
 if __name__ == "__main__":
     init_pop = Population[Binary]()
 
-    for i in range (0, 1):
+    for i in range(0, 1):
         init_pop.append(Binary.create_random(10))
 
     evaluator = BitDistanceEvaluator()
@@ -100,19 +107,18 @@ if __name__ == "__main__":
     cselector = Elitist(SimpleSelector[Binary](10))
     variator = RandomBitMutator()
 
-    ctrl = LinearController (
-        population = init_pop,
-        parent_evaluator = evaluator,
-        parent_selector = pselector,
-        variator = variator,
-        survivor_evaluator = evaluator,
-        survivor_selector = cselector,
+    ctrl: LinearController = LinearController(
+        population=init_pop,
+        parent_evaluator=evaluator,
+        parent_selector=pselector,
+        variator=variator,
+        survivor_evaluator=evaluator,
+        survivor_selector=cselector,
     )
 
-    dicts : typing.Dict[int, Optional[float]]= {}
+    dicts: typing.Dict[int, Optional[float]] = {}
 
     from core.accountant import Accountant
-
 
     # Ignore type checking. Because `Accountant` is defined for the
     #       `Controller` class, which does not necessarily have one
@@ -120,8 +126,8 @@ if __name__ == "__main__":
     # Because an LinearController is registered with this `Accountant`,
     #       we can be _somewhat_ sure that the accountant's suject has
     #       a `.population`.
-    #       
-    acc = Accountant({"GENERATION_BEGIN": lambda x : len(x.population)}) # type:ignore
+    #
+    acc = Accountant({"GENERATION_BEGIN": lambda x: len(x.population)})  # type:ignore
 
     ctrl.register(acc)
 
@@ -129,6 +135,6 @@ if __name__ == "__main__":
         ctrl.step()
         dicts[i] = ctrl.population[0].fitness
 
-    print (dicts)
+    print(dicts)
 
     print(acc.publish())
