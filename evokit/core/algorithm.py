d904fd96a40a1b47532b05e663996d3ef52f23ba
lyodine <commit@lyodine.com>
2025-12-30 14:15:52 +0800

    fix(watching): Amend 617e658

--- evokit/core/algorithm.py
+++ evokit/core/algorithm.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
     from typing import Any
     from typing import Type
     from typing import Callable
-    from ..accounting import Watcher
+    from ..watching import Watcher
 
 
 class _MetaAlgorithm(ABCMeta):
