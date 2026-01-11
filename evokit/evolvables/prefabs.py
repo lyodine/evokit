667c55dd0a3a5475fed3dfc91218c8cde6893be6
lyodine <commit@lyodine.com>
2026-01-11 20:55:05 +0800

    feat(inspect): Add tools for extracting default parameter values

--- evokit/evolvables/prefabs.py
+++ evokit/evolvables/prefabs.py
@@ -3,16 +3,15 @@ from evokit.evolvables.algorithms import SimpleLinearAlgorithm
 from evokit.evolvables.selectors import TruncationSelector, Elitist
 from evokit.evolvables.bitstring import BitString, CountBits, MutateBits
 from evokit.tools.lineage import TrackParents
-from inspect import signature
+from evokit._utils.inspect import get_default_value
+
 
 """So, this global variable, ahh... fetches the default value
 of :arg:`max_parents` in TrackParents's constructor. Because
 apparently Python lets you do that.
 """
 _TRACK_PARENTS_MAX_PARENTS_DEFAULT: int =\
-    [para.default
-     for para in signature(TrackParents).parameters.values()
-     if para.name == "max_parents"][0]
+    get_default_value(TrackParents, "max_parents")
 
 
 def make_onemax(pop_size: int,
@@ -29,7 +28,7 @@ def make_onemax(pop_size: int,
         BitString.random(ind_size) for _ in range(pop_size))
     return SimpleLinearAlgorithm(population=pop,
                                  variator=TrackParents(
-                                     MutateBits(mutate_p),
+                                     MutateBits(mutate_p, processes=processes),
                                      max_parents=max_parents),
                                  evaluator=CountBits(),
                                  selector=Elitist(
